8f7c958a1a1b5943b43599a56173c051002b9fa4
hypewsthedev <joehosten@outlook.com>
2023-03-10 00:35:50 +0000

    update

--- src/main/java/me/joehosten/illusivestafftracker/commands/DiscordTestCommand.java
+++ src/main/java/me/joehosten/illusivestafftracker/commands/DiscordTestCommand.java
@@ -1,10 +1,8 @@
 package me.joehosten.illusivestafftracker.commands;
 
-import games.negative.framework.db.SQLDatabase;
 import games.negative.framework.discord.command.SlashCommand;
 import games.negative.framework.discord.command.SlashInfo;
 import me.joehosten.illusivestafftracker.Bot;
-import me.joehosten.illusivestafftracker.IllusiveStaffTracker;
 import me.joehosten.illusivestafftracker.core.util.DbUtils;
 import me.joehosten.illusivestafftracker.core.util.DiscordSrvUtils;
 import me.joehosten.illusivestafftracker.core.util.TimeUtil;
@@ -12,9 +10,6 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -27,25 +22,24 @@ public class DiscordTestCommand extends SlashCommand {
         if (!slashCommandInteractionEvent.getUser().getId().equals("462296411141177364")) return;
         LocalDateTime now = LocalDateTime.now();
 
-        if (now.getHour() == 11) {
-            // discord stuff
-            StringBuilder toSend = new StringBuilder();
-            ArrayList<String> missedQuota = new ArrayList<>();
-            for (String uuid : DbUtils.getAllUuids()) {
-                long rawPlayTime = Long.parseLong(DbUtils.getCurrentTime(uuid)) - Long.parseLong(DbUtils.getAfkTime(uuid));
-                String playTime = TimeUtil.format(rawPlayTime, 0);
-                String afkTime = TimeUtil.format(Long.parseLong(DbUtils.getAfkTime(uuid)), 0);
-                toSend.append("- **").append(DiscordSrvUtils.getUser(UUID.fromString(uuid)).getAsMention()).append("** has actively played **").append(playTime).append("** so far this week and AFK'd for **%afk%**.".replace("%afk%", afkTime)).append("\n");
-                if (DiscordSrvUtils.calculateMissedQuota(uuid) != 0) missedQuota.add(uuid);
-            }
-            toSend.append("\n").append("Staff need to meet **six hours** of playtime before they receive a strike. \n\nBelow are the members that have not met this requirement yet.\n\n");
-            missedQuota.forEach(k -> toSend.append(DiscordSrvUtils.getUser(UUID.fromString(k)).getAsMention()).append(" - Missed quota by: **").append(TimeUtil.format(DiscordSrvUtils.calculateMissedQuota(k), 0).replaceAll("-", "")).append("**\n"));
-
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setTitle("Daily Summary");
-            eb.setDescription(toSend);
-            TextChannel tc = Bot.getBot().getJda().getTextChannelById("1014041763037581342");
-            Objects.requireNonNull(tc).sendMessageEmbeds(eb.build()).queue();
+        // discord stuff
+        StringBuilder toSend = new StringBuilder();
+        ArrayList<String> missedQuota = new ArrayList<>();
+        for (String uuid : DbUtils.getAllUuids()) {
+            long rawPlayTime = Long.parseLong(DbUtils.getCurrentTime(uuid)) - Long.parseLong(DbUtils.getAfkTime(uuid));
+            String playTime = TimeUtil.format(rawPlayTime, 0);
+            String afkTime = TimeUtil.format(Long.parseLong(DbUtils.getAfkTime(uuid)), 0);
+            toSend.append("- **").append(DiscordSrvUtils.getUser(UUID.fromString(uuid)).getAsMention()).append("** has actively played **").append(playTime).append("** so far this week and AFK'd for **%afk%**.".replace("%afk%", afkTime)).append("\n");
+            if (DiscordSrvUtils.calculateMissedQuota(uuid) != 0) missedQuota.add(uuid);
         }
+        toSend.append("\n").append("Staff need to meet **six hours** of playtime before they receive a strike. \n\nBelow are the members that have not met this requirement yet.\n\n");
+        missedQuota.forEach(k -> toSend.append(DiscordSrvUtils.getUser(UUID.fromString(k)).getAsMention()).append(" - Missed quota by: **").append(TimeUtil.format(DiscordSrvUtils.calculateMissedQuota(k), 0).replaceAll("-", "")).append("**\n"));
+
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setTitle("Daily Summary");
+        eb.setDescription(toSend);
+        TextChannel tc = Bot.getBot().getJda().getTextChannelById("1014041763037581342");
+        Objects.requireNonNull(tc).sendMessageEmbeds(eb.build()).queue();
     }
 }
+
